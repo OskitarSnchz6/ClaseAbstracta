01fd4b9088e05af7672707f50e4a88d7ba7eeed9
OskitarSnchz6 <oskitarsnchz6@gmail.com>
2024-05-23 14:51:17 +0200

    update

---
+++ ClaAbstractas/src/claabstractas/Cuadrado.java
@@ -0,0 +1,25 @@
+package claabstractas;
+import java.util.Scanner;
+
+public class Cuadrado extends Figura {
+    private double lado;
+
+    public Cuadrado() {
+    }
+
+    public Cuadrado(double x, double y, double lado) {
+        super(x, y);
+        this.lado = lado;
+    }
+
+    @Override
+    public double calcularArea() {
+        return lado * lado;
+    }
+
+    @Override
+    public void ingresarDatos(Scanner sc) {
+        System.out.print("Ingrese el lado del cuadrado: (Escribe ',' para los decimales)");
+        this.lado = sc.nextDouble();
+    }
+}
